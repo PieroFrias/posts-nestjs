9eae36c690151a7f0e2c39bb06bd556029ecf16b
PieroFrias <piefrimo@gmail.com>
2025-11-20 17:55:09 -0500

    feat: Refactor CategoriesService to implement CRUD operations and status change functionality

--- src/modules/categories/categories.service.ts
+++ src/modules/categories/categories.service.ts
@@ -1,26 +1,91 @@
 import { Injectable } from '@nestjs/common';
-import { CreateCategoryDto } from './dto/create-category.dto';
-import { UpdateCategoryDto } from './dto/update-category.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Category } from './entities';
+import { CreateCategoryDto, FilterCategoryDto, UpdateCategoryDto } from './dto';
+import { checkCategoryExists, buildQueryCategories } from './helpers';
+import {
+  ChangeStatusResponse,
+  CreateResponse,
+  FindAllResponse,
+  UpdateResponse,
+} from '../../common/interfaces';
+import { ErrorMessage, SuccessMessage } from '../../common/utils';
+import { GeneralStatus } from 'src/common/constants';
 
 @Injectable()
 export class CategoriesService {
-  create(createCategoryDto: CreateCategoryDto) {
-    return 'This action adds a new category';
+  constructor(
+    @InjectRepository(Category)
+    private readonly repo: Repository<Category>,
+  ) {}
+
+  async create(createDto: CreateCategoryDto): Promise<CreateResponse<Category>> {
+    const { name } = createDto;
+
+    await checkCategoryExists(this.repo, name);
+
+    let newCategory = this.repo.create(createDto);
+    newCategory = await this.repo.save(newCategory);
+
+    return {
+      data: newCategory,
+      message: SuccessMessage.created('Category', name),
+    };
   }
 
-  findAll() {
-    return `This action returns all categories`;
+  async findAll(filterDto: FilterCategoryDto): Promise<FindAllResponse<Category>> {
+    const { page = 1, pageSize = 10 } = filterDto;
+
+    const [data, totalItems] = await buildQueryCategories(this.repo, filterDto);
+
+    const totalPages = Math.ceil(totalItems / pageSize);
+
+    return {
+      data,
+      currentPage: page,
+      totalPages,
+      totalItems,
+    };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} category`;
+  async findOne(id: string): Promise<Category> {
+    const category = await this.repo.findOne({ where: { id } });
+
+    !category && ErrorMessage.notFound('Category', id);
+
+    return category;
   }
 
-  update(id: number, updateCategoryDto: UpdateCategoryDto) {
-    return `This action updates a #${id} category`;
+  async update(id: string, updatedDto: UpdateCategoryDto): Promise<UpdateResponse<Category>> {
+    const { name } = updatedDto;
+    const category = await this.findOne(id);
+
+    await checkCategoryExists(this.repo, name, id);
+
+    let updatedCategory = this.repo.merge(category, updatedDto);
+    updatedCategory = await this.repo.save(updatedCategory);
+
+    return {
+      data: updatedCategory,
+      message: SuccessMessage.updated('Category', id),
+    };
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} category`;
+  async changeStatus(id: string): Promise<ChangeStatusResponse<Category>> {
+    const category = await this.findOne(id);
+
+    const currentStatus = category.status;
+    const isActive = currentStatus === GeneralStatus.ACTIVE;
+
+    category.status = isActive ? GeneralStatus.INACTIVE : GeneralStatus.ACTIVE;
+    await this.repo.save(category);
+
+    const newStatus = category.status;
+
+    return {
+      data: category,
+      message: SuccessMessage.statusChanged('Staff Category', id, newStatus),
+    };
   }
 }
