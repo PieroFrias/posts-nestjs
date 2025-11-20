9ae64ce32e30ccea035fd4a74dc67e0b972ec48c
PieroFrias <piefrimo@gmail.com>
2025-11-20 14:25:34 -0500

    feat: Add module imports for Users, Posts, Categories, Tags, PostTags, and Comments

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,25 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config';
+import {
+  UsersModule,
+  PostsModule,
+  TagsModule,
+  PostTagsModule,
+  CommentsModule,
+  CategoriesModule,
+} from './modules';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
     TypeOrmModule.forRootAsync(typeOrmConfig),
+    UsersModule,
+    PostsModule,
+    CategoriesModule,
+    TagsModule,
+    PostTagsModule,
+    CommentsModule,
   ],
   controllers: [],
   providers: [],
