ff3c7e7d19ecf83ee66f3b50ad42d87a060846aa
PieroFrias <piefrimo@gmail.com>
2025-11-20 18:57:16 -0500

    feat: Implement PostTagsService with tag association and deletion methods

--- src/modules/post-tags/post-tags.module.ts
+++ src/modules/post-tags/post-tags.module.ts
@@ -1,12 +1,15 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostTag } from './entities';
 import { PostTagsService } from './post-tags.service';
 import { PostTagsController } from './post-tags.controller';
+import { TagsModule } from '../tags/tags.module';
+import { PostsModule } from '../posts/posts.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PostTag])],
+  imports: [TypeOrmModule.forFeature([PostTag]), forwardRef(() => PostsModule), TagsModule],
   controllers: [PostTagsController],
   providers: [PostTagsService],
+  exports: [PostTagsService],
 })
 export class PostTagsModule {}
--- src/modules/post-tags/post-tags.service.ts
+++ src/modules/post-tags/post-tags.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { PostTag } from './entities/post-tag.entity';
+import { Post } from '../posts/entities';
+import { TagsService } from '../tags/tags.service';
 
 @Injectable()
-export class PostTagsService {}
+export class PostTagsService {
+  constructor(
+    @InjectRepository(PostTag)
+    private postTagsRepository: Repository<PostTag>,
+    private tagsService: TagsService,
+  ) {}
+
+  async associateTagsWithPost(post: Post, tagNames: string[], postId?: string): Promise<void> {
+    if (!tagNames?.length) return;
+
+    postId && this.deleteTagsByPost(postId);
+
+    const tags = await this.tagsService.findOrCreate(tagNames);
+
+    await Promise.all(
+      tags.map(async (tag) => {
+        const postTag = this.postTagsRepository.create({ post, tag });
+        await this.postTagsRepository.save(postTag);
+      }),
+    );
+  }
+
+  async deleteTagsByPost(postId: string): Promise<void> {
+    await this.postTagsRepository.delete({ post: { id: postId } });
+  }
+}
