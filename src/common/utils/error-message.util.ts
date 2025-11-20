d9bc4da3c8b6413d6dfa1072118cf303bfd7efa9
PieroFrias <piefrimo@gmail.com>
2025-11-20 17:00:13 -0500

    feat: Add notFound method to ErrorMessage utility for improved error handling

--- src/common/utils/error-message.util.ts
+++ src/common/utils/error-message.util.ts
@@ -1,9 +1,27 @@
-import { BadRequestException, ConflictException, UnauthorizedException } from '@nestjs/common';
+import {
+  BadRequestException,
+  ConflictException,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 
 export class ErrorMessage {
   static alreadyRegistered(property: string, value: string): never {
     const message = `${property} '${value}' is already registered.`;
     throw new ConflictException(message);
+  }
+
+  static notFound(entity: string, entityId?: string): never {
+    const message = entityId
+      ? `${entity} with ID '${entityId}' not found.`
+      : `${entity} not found.`;
+
+    throw new NotFoundException(message);
+  }
+
+  static notAuthorized(): never {
+    const message = `You are not authorized to perform this action.`;
+    throw new UnauthorizedException(message);
   }
 
   static noMatchPassword(): never {
