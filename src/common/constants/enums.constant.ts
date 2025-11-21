2fe23cd32ba0bb918706dd9d6814616231acc85e
PieroFrias <piefrimo@gmail.com>
2025-11-20 21:43:08 -0500

    feat: Refactor BaseStatus enum to include PUBLISHED and update PostStatus accordingly

--- src/common/constants/enums.constant.ts
+++ src/common/constants/enums.constant.ts
@@ -1,6 +1,18 @@
-export enum GeneralStatus {
+export enum BaseStatus {
+  DRAFT = 'DRAFT',
   ACTIVE = 'ACTIVE',
   INACTIVE = 'INACTIVE',
+  PUBLISHED = 'PUBLISHED',
+}
+
+export enum GeneralStatus {
+  ACTIVE = BaseStatus.ACTIVE,
+  INACTIVE = BaseStatus.INACTIVE,
+}
+
+export enum PostStatus {
+  DRAFT = BaseStatus.DRAFT,
+  PUBLISHED = BaseStatus.PUBLISHED,
 }
 
 export enum Role {
